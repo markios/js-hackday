dc37c71d1b51684a6c2cce505e0e5a4512024141
Mieszko Kycermann <59399685+Kycermann@users.noreply.github.com>
2021-04-07 17:07:47 +0100

    Change userform & app styles

--- src/client/components/userForm/userForm.js
+++ src/client/components/userForm/userForm.js
@@ -1,6 +1,10 @@
 import {
   useState
 } from 'react';
+
+import Button from "../Button";
+import Panel from "../Panel";
+
 import './userForm.css';
 
 const onChangeWrapper = (setter) => (e) => setter(e.currentTarget.value);
@@ -16,15 +20,18 @@ const UserForm = ({ onSubmit }) => {
 
   return (
     <div className="user-form purple-background">
-      <h2 className="user-form__header">User Form</h2>
-      <form onSubmit={onSubmitHandler}>
-        <div className="user-form__wrapper">
-          <input required type="text" value={name} onChange={onChangeWrapper(setName)} name="name" placeholder="Name" />
-          <input required type="color" value={avatar} onChange={onChangeWrapper(setAvatar)} name="avatar" />
-          <label htmlFor="avatar">Colour</label>
-        </div>
-        <button>Submit</button>
-      </form>
+      <h2 className="user-form__header">Welcome to SongHack</h2>
+      <div className="space"></div>
+      <div className="user-form__wrapper">
+        <label htmlFor="name">Nickname:</label>
+        <input required type="text" value={name} onChange={onChangeWrapper(setName)} name="name" placeholder="Parrot Pirate" />
+        <br/>
+        <label htmlFor="avatar">Colour:</label><br/>
+        <input required type="color" value={avatar} onChange={onChangeWrapper(setAvatar)} name="avatar" />
+        <br/><br/>
+        <Button onClick={onSubmitHandler}>Play</Button>
+        <br/>
+      </div>
     </div>
   )
 }
