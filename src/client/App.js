b431d1e3ae5d123d5f4818a361694630bc9b5395
Mieszko Kycermann <59399685+Kycermann@users.noreply.github.com>
2021-04-07 11:57:27 +0100

    Add song API

--- src/client/App.js
+++ src/client/App.js
@@ -2,26 +2,18 @@ import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { playTrack } from "./util/songs";
+
 function App() {
-  useEffect(() => {
-    fetch('/game').then(d => d.json()).then((d) => console.log(d));
-  }, []);
+  
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={() => playTrack("975918478", 10)}>
+          Click to play
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/client/util/songs.js
@@ -0,0 +1,61 @@
+let playing = false;
+let wrapper = null;
+
+function init() {
+    wrapper = document.createElement("div");
+    wrapper.style.setProperty("position", "fixed");
+    wrapper.style.setProperty("top", "-10000px");
+    document.body.appendChild(wrapper);
+}
+
+async function playTrack(songId, seconds=10) {
+    playing = true;
+
+    const songUrl = [
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/",
+        songId,
+        "&auto_play=true",
+        "&visual=false"
+    ].join("");
+
+    // Start song
+    wrapper.innerHTML = `
+        <iframe
+            id="song-frame"
+            width="100%"
+            height="300"
+            scrolling="no"
+            frameborder="no"
+            allow="autoplay"
+            src="${songUrl}"></iframe>
+    `;
+    
+    const songFrame = document.getElementById("song-frame");
+
+    // Wait for it to load
+    await new Promise((res, rej) => {
+        songFrame.addEventListener("load", () => res());
+    });
+
+    // Let song play for a bit
+    await new Promise(res => setTimeout(res, seconds * 1000));
+
+    // Stop song
+    wrapper.innerHTML = "";
+    playing = false;
+}
+
+function isPlayingTrack() {
+    return playing;
+}
+
+function stopTrack() {
+    if(playing) {
+        wrapper.innerHTML = "";
+        playing = false;
+    }
+}
+
+init();
+
+export { playTrack, isPlayingTrack, stopTrack };
