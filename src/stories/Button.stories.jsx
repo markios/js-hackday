54ecff979362996203216824e6f9579063a3f01b
Mark Kellett <markkellett@bulb.co.uk>
2022-01-18 09:30:25 +0000

    --wip-- [skip ci]

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Button from "../client/components/Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/Button",
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    children: { control: "text" },
+    disabled: { control: "boolean" },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  children: "Hello World",
+  disabled: false,
+  onClick: () => alert("hello world"),
+};
